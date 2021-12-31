e229b8ede8710d1a74a274d8d4c13407960cad7b
Klaus Wriessnegger <klaus.wriessnegger@conology.net>
2021-12-31 21:56:25 +0100

    cleanup

--- src/main/java/org/openapi2puml/DemoApplication.java
+++ src/main/java/org/openapi2puml/DemoApplication.java
@@ -1,24 +1,14 @@
 package org.openapi2puml;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.annotation.SessionScope;
-import org.zkoss.zkspringboot.ZkProperties;
-
-import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
 @Log4j2
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
